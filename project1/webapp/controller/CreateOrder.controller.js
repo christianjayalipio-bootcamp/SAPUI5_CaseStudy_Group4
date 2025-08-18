39b10c8289592c0e50c0c77d3f2e6e8199e1c4bb
Christian Jay Alipio <christian.jay.alipio@accenture.com>
2025-08-18 08:25:08 +0000

    commit for update

---
+++ project1/webapp/controller/CreateOrder.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.sapui5.casestudy.group4.project1.controller.CreateOrder", {
+        onInit() {
+        }
+    });
+});
--- project1/webapp/controller/MainView.controller.js
+++ project1/webapp/controller/MainView.controller.js
@@ -1,10 +1,34 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent"
+], function (Controller, UIComponent) {
     "use strict";
 
     return Controller.extend("com.sapui5.casestudy.group4.project1.controller.MainView", {
-        onInit() {
-        }
+        onInit: function () {
+            // Initialization logic if needed
+        },
+
+        // Triggered when "Add" button in toolbar is pressed
+        onAddOrder: function () {
+            var oRouter = UIComponent.getRouterFor(this);
+            oRouter.navTo("RouteView2"); // Navigate to target route
+        },
+
+        // Triggered when row "Details" button is pressed
+        // onRowNavigate: function (oEvent) {
+        //     var oContext = oEvent.getSource().getBindingContext();
+        //     if (oContext) {
+        //         var sOrderNumber = oContext.getProperty("OrderNumber"); // adjust property name
+        //         var oRouter = UIComponent.getRouterFor(this);
+        //         oRouter.navTo("OrderDetail", {
+        //             orderId: sOrderNumber   // pass order ID as parameter
+        //         });
+        //     } else {
+        //         // fallback if no context
+        //         var oRouter = UIComponent.getRouterFor(this);
+        //         oRouter.navTo("OrderDetail");
+        //     }
+        // }
     });
 });
