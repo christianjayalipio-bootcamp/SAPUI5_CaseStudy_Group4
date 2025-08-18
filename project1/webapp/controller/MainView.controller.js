7c65d87451604d168ea903382b22ace064162bf5
Cloyd Wally Asuncion <cloyd.wally.asuncion@accenture.com>
2025-08-18 08:18:02 +0000

    Initialize Create Page View commit

--- project1/webapp/controller/MainView.controller.js
+++ project1/webapp/controller/MainView.controller.js
@@ -4,7 +4,16 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("com.sapui5.casestudy.group4.project1.controller.MainView", {
-        onInit() {
+        onInit: function () {
+            const oPage = this.byId("page0");
+            if (oPage) {
+                oPage.setFloatingFooter(!oPage.getFloatingFooter());
+            }
+        },
+
+        onCreateOrder: function () {
+            const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("RouteCreateOrder");
         }
     });
 });
