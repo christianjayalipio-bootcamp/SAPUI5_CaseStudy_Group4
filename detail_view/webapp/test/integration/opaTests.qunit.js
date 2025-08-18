4f3727b690dc9196dc75bd0060d819d198bc1e3d
christianjayalipio-bootcamp <christian.jay.alipio@accenture.com>
2025-08-18 17:09:05 +0800

    Delete detail_view directory

--- detail_view/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-QUnit.config.autostart = false;
-
-sap.ui.require(["detailview/test/integration/AllJourneys"
-], function () {
-	QUnit.start();
-});
--- detail_view/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"detailview/test/unit/controller/DetailView.controller"
-], function () {
-	"use strict";
-});
